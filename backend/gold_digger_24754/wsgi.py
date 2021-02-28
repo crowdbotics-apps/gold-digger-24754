e0bd7fd831adadd1eea15ffd8151ad2f290c891f
Crowdbotics <team@crowdbotics.com>
2021-02-28 02:18:59 +0000

    Update models

--- backend/gold_digger_24754/wsgi.py
+++ backend/gold_digger_24754/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gold_digger_24754.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gold_digger_24754.settings")
 
 application = get_wsgi_application()
